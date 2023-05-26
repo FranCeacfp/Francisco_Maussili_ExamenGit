f0b233ed9dbd01625312497e15ca455600bf849d
FranciscoMaussili-Al <FranciscoMaussili-Al@CEACFP10328>
2023-05-26 13:01:47 +0200

    Probando cosas

--- Examen_Git/src/pkg/Clase.java
+++ Examen_Git/src/pkg/Clase.java
@@ -5,6 +5,7 @@ public class Clase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("K dise colega, no se porque no me funciona el eclipse en mi ordenador");
+		No se porque me hace cosas raras
 	}
 
 }
