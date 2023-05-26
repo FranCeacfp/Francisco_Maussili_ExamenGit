0d0b520c008e03a0be582961c0d9a06a28361218
FranciscoMaussili-Al <FranciscoMaussili-Al@CEACFP10328>
2023-05-26 12:45:41 +0200

    En la rama secundaria modificamos el archivo clase y creamos clase dos en el paquete nuevo pkg2

--- Examen_Git/src/pkg/Clase.java
+++ Examen_Git/src/pkg/Clase.java
@@ -4,7 +4,7 @@ public class Clase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("K dise colega");
+		System.out.println("K dise colega, no se porque no me funciona el eclipse en mi ordenador");
 	}
 
 }
