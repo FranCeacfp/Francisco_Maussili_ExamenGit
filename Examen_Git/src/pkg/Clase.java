76adbaed7b6abdbe39837ddbe2dcba847cf3d303
FranciscoMaussili-Al <FranciscoMaussili-Al@CEACFP10328>
2023-05-26 13:11:04 +0200

    Modificamos el archivo de la rama 3

--- Examen_Git/src/pkg/Clase.java
+++ Examen_Git/src/pkg/Clase.java
@@ -5,7 +5,7 @@ public class Clase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("K dise colega, no se porque no me funciona el eclipse en mi ordenador");
+		System.out.println("K dise colega, no se porque no me funciona el eclipse en mi ordenador y quiero llorar");
 
 
 	}
