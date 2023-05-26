e3e030476361e1473f51d96b5e424e8500d1816b
FranciscoMaussili-Al <FranciscoMaussili-Al@CEACFP10328>
2023-05-26 12:56:24 +0200

    En la rama tres creo otro paquete con otra clase nueva

---
+++ Examen_Git/src/pkg2/clase2.java
@@ -0,0 +1,10 @@
+package pkg2;
+
+public class clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pon facil el examen del lunes porfa");
+	}
+
+}
